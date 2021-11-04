724cfe76538e5201855b49c96ef15ac936c19d51
Neeraj Verma <neerajverma101@gmail.com>
2021-11-04 09:47:43 +0530

    added env port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routes = require("./routes/app");
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use("/", routes);
